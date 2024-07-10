1948d5d64096f173ed9d327e51f0cf1015f321b8
bellobambo <bellobambo21@gmail.com>
2024-07-10 10:12:05 +0100

    mysql

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json()); // parse json bodies in the request object
 
 // Redirect requests to endpoint starting with /posts to postRoutes.js
-// app.use("/posts", require("./routes/postRoutes"));
+app.use("/posts", require("./routes/postRoutes"));
 
 // Global Error Handler. IMPORTANT function params MUST start with err
 app.use((err, req, res, next) => {
@@ -23,3 +23,5 @@ app.use((err, req, res, next) => {
 // Listen on pc port
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`));
+
+
